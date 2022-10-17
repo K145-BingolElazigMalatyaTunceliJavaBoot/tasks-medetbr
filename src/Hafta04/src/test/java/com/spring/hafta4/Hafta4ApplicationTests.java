c47c802e00ca003eff2ec4eadd9a5ac9c3a802d1
Medet Berimen <74211970+medetbr@users.noreply.github.com>
2022-10-17 11:23:38 +0300

    Hafta04

---
+++ src/Hafta04/src/test/java/com/spring/hafta4/Hafta4ApplicationTests.java
@@ -0,0 +1,13 @@
+package Hafta04.src.test.java.com.spring.hafta4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hafta4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
